6d2dd01adc27ec9706d59b630c6fa36c32965913
Arazpee <arazpee@gmail.com>
2019-04-05 23:30:14 +0700

    the boilerplate styleguidist with bootstrap attached in it

---
+++ src/components/CardBox/Readme.md
@@ -0,0 +1,3 @@
+```jsx
+<CardBox></CardBox>
+```
---
+++ styleguide.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  template: {
+    head: {
+      scripts: [
+        {
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.4/js/mdb.min.js'
+        },
+        {
+          src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js'
+        },
+        {
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'
+        },
+        {
+          src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+        }
+      ],
+      links: [
+        {
+          rel: 'stylesheet',
+          href:
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+        },
+        {
+          rel: 'stylesheet',
+          href:
+            'https://cdnjs.cloudflare.com/ajax/libs/froala-design-blocks/2.0.0/css/froala_blocks.min.css'
+        },
+        {
+          rel: 'stylesheet',
+          href:
+            'https://use.fontawesome.com/releases/v5.3.1/css/all.css'
+        },
+        {
+          rel: 'stylesheet',
+          href:
+            'https://cdnjs.cloudflare.com/ajax/libs/froala-design-blocks/2.0.0/css/froala_blocks.min.css'
+        },
+        {
+          rel: 'stylesheet',
+          href:
+            'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.4/css/mdb.min.css'
+        }
+      ]
+    }
+  }
+}
